511213c9174211ecc23af5f97834012853552b88
franc <francyardol@gamil.com>
2025-05-27 18:20:43 +0200

    aggiunta DTO

--- SistemaGesioneEventi/src/DTO/DTO.java
+++ SistemaGesioneEventi/src/DTO/DTO.java
@@ -6,33 +6,89 @@ public class DTO {
     private String data;
     private String ora;
     private String luogo;
-    private String
+    private String NumPartecipanti;
+    private String NumMaxPartecipanti;
 
-    public myDTO() {
+    public DTO() {
     }
 
-    public myDTO(String campo1, String campo2) {
-        this.campo1 = campo1;
-        this.campo2 = campo2;
+    public DTO(String titolo, String descrizione, String data, String ora, String luogo, String numPartecipanti, String numMaxPartecipanti) {
+        this.titolo = titolo;
+        this.descrizione = descrizione;
+        this.data = data;
+        this.ora = ora;
+        this.luogo = luogo;
+        NumPartecipanti = numPartecipanti;
+        NumMaxPartecipanti = numMaxPartecipanti;
     }
 
-    public String getCampo1() {
-        return this.campo1;
+
+    public String getTitolo() {
+        return titolo;
     }
 
-    public void setCampo1(String campo1) {
-        this.campo1 = campo1;
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
     }
 
-    public String getCampo2() {
-        return this.campo2;
+    public String getDescrizione() {
+        return descrizione;
     }
 
-    public void setCampo2(String campo2) {
-        this.campo2 = campo2;
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
     }
 
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getOra() {
+        return ora;
+    }
+
+    public void setOra(String ora) {
+        this.ora = ora;
+    }
+
+    public String getLuogo() {
+        return luogo;
+    }
+
+    public void setLuogo(String luogo) {
+        this.luogo = luogo;
+    }
+
+    public String getNumPartecipanti() {
+        return NumPartecipanti;
+    }
+
+    public void setNumPartecipanti(String numPartecipanti) {
+        NumPartecipanti = numPartecipanti;
+    }
+
+    public String getNumMaxPartecipanti() {
+        return NumMaxPartecipanti;
+    }
+
+    public void setNumMaxPartecipanti(String numMaxPartecipanti) {
+        NumMaxPartecipanti = numMaxPartecipanti;
+    }
+
+    @Override
     public String toString() {
-        return "myDTO [campo1=" + this.campo1 + ", campo2=" + this.campo2 + "]";
+        return "DTO{" +
+                "titolo='" + titolo + '\'' +
+                ", descrizione='" + descrizione + '\'' +
+                ", data='" + data + '\'' +
+                ", ora='" + ora + '\'' +
+                ", luogo='" + luogo + '\'' +
+                ", NumPartecipanti='" + NumPartecipanti + '\'' +
+                ", NumMaxPartecipanti='" + NumMaxPartecipanti + '\'' +
+                '}';
     }
 }
